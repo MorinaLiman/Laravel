bddfed52d510784c52473fb58f86f3422c2ae471
MorinaLiman <159453787+MorinaLiman@users.noreply.github.com>
2024-12-04 20:33:21 +0100

    Add files via upload

---
+++ 1376f40015d114cab02cc6b42e6a6a78.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+       
+   
+        <title>Detyra 1</title>
+
+        <meta charset="utf-8">
+        <link href=data;, rel=icon>
+
+      
+    </head>
+    <body>
+        <h1>Detyra 1</h1>
+
+        <!-- Tabela me emrin, mbiemrin dhe universitetin -->
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>Emri</th>
+                    <th>Mbiemri</th>
+                    <th>Universiteti</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Liman </td>
+                    <td>Morina</td>
+                    <td>Ukshin Hoti</td>
+                </tr>
+                <tr>
+                    <td>Ardi</td>
+                    <td>Kryeziu</td>
+                    <td>Hasan Prishtina</td>
+                </tr>
+                <tr>
+                    <td>Elmedina</td>
+                    <td>Gashi</td>
+                    <td>UBT</td>
+                </tr>
+            </tbody>
+        </table>
+
+   
+    </body>
+</html>
+<?php /**PATH C:\laragon\www\Detyra-1\resources\views/welcome.blade.php ENDPATH**/ ?>
---
+++ welcome.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+       
+   
+        <title>Detyra 1</title>
+
+        <meta charset="utf-8">
+        <link href=data;, rel=icon>
+
+      
+    </head>
+    <body>
+        <h1>Detyra 1</h1>
+
+        <!-- Tabela me emrin, mbiemrin dhe universitetin -->
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>Emri</th>
+                    <th>Mbiemri</th>
+                    <th>Universiteti</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Liman </td>
+                    <td>Morina</td>
+                    <td>Ukshin Hoti</td>
+                </tr>
+                <tr>
+                    <td>Ardi</td>
+                    <td>Kryeziu</td>
+                    <td>Hasan Prishtina</td>
+                </tr>
+                <tr>
+                    <td>Elmedina</td>
+                    <td>Gashi</td>
+                    <td>UBT</td>
+                </tr>
+            </tbody>
+        </table>
+
+   
+    </body>
+</html>
